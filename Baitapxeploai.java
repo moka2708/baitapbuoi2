6e8c780017c30344410f644119d696435883e5bb
moka2708 <aolaoxao123@gmail.com>
2026-01-11 14:52:09 +0700

    Update Baitapxeploai.java

--- Baitapxeploai.java
+++ Baitapxeploai.java
@@ -4,30 +4,22 @@ import java.util.Scanner;
 public class Baitapxeploai {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("--- XEP LOAI HOC LUC ---");
-
         System.out.print("Nhap diem tong ket (0 - 10): ");
         double diem = scanner.nextDouble();
         if (diem < 0 || diem > 10) {
             System.out.println("LOI: Diem phai nam trong khoang tu 0 den 10!");
-        } else {
-                    
+        } else {                    
             if (diem >= 9) {
-
                 System.out.println("Ket qua: Xep loai GIOI");
-            } else if (diem >= 7) {
-     
+            } else if (diem >= 7) {     
                 System.out.println("Ket qua: Xep loai KHA");
-            } else if (diem >= 5) {
-             
+            } else if (diem >= 5) {             
                 System.out.println("Ket qua: Xep loai TRUNG BINH");
-            } else {
-            
+            } else {            
                 System.out.println("Ket qua: Xep loai YEU");
             }
         }
-
         scanner.close();
     }
+
 }
