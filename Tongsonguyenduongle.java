855432fcc0b4bc7e27daff7276149a04ceec4c8f
moka2708 <aolaoxao123@gmail.com>
2026-01-11 14:48:48 +0700

    Update Tongsonguyenduongle.java

--- Tongsonguyenduongle.java
+++ Tongsonguyenduongle.java
@@ -2,23 +2,17 @@
 package tongsonguyenduongle;
 import java.util.Scanner;
 public class Tongsonguyenduongle {
-
-
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Nhap n: ");
         int n = sc.nextInt();
-
-        long tong = 0;
-        int i = 1; // Bắt đầu từ số lẻ đầu tiên
-
+        long S = 0;
+        int i = 1;
         while (i <= n) {
-            tong += i;
-            i += 2; // Tăng thêm 2 để nhảy sang số lẻ tiếp theo (1 -> 3 -> 5...)
+            S += i;
+            i += 2; 
         }
-
-        System.out.println("Tong cac so le = " + tong);
+        System.out.println("Tong cac so le = " + S);
     }
+
 }
